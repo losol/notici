4ab17c73092846168170ec66f3209990dd4be6e6
Ole Kristian Losvik <losolio@github.user>
2019-03-03 14:30:49 +0100

    Added archive page

--- public/templates/archive-notici.php
+++ public/templates/archive-notici.php
@@ -26,16 +26,12 @@ get_header();
 			$args = [
 				'post_status'    => 'publish',
 				'post_type'      => 'notici',
-				'posts_per_page' => 100,
-				'orderby'        => 'meta_value',
-				'order'          => 'ASC',
-				'meta_type'      => 'DATE',
-				'meta_key'       => 'notici_startdate',
+				'posts_per_page' => 50,
+				'orderby'        => 'publish_date',
+				'order'          => 'DESC',
 			];
 
 			$posts         = new WP_Query( $args );
-			$current_year  = '';
-			$current_month = '';
 			while ( $posts->have_posts() ) {
 			?>
 			<article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -44,40 +40,13 @@ get_header();
 				$posts->the_post();
 				$post_id = get_the_id();
 
-				$startdate  = strtotime( get_post_meta( $post_id, 'notici_startdate', true ) );
-				$enddate    = strtotime( get_post_meta( $post_id, 'notici_enddate', true ) );
-				$dateformat = get_option( 'date_format' );
-
-				$last_year    = $current_year;
-				$last_month   = $current_month;
-				$current_year = date_i18n( 'Y', $startdate );
-				if ( $last_year != $current_year ) {
-					$last_month = '';
-				}
-				$current_month = date_i18n( 'F', $startdate );
-
-				if ( $last_year != $current_year ) {
-					echo '<h2>' . $current_year . '</h2>';
-				}
-
-				if ( $last_month != $current_month ) {
-					echo '<h3>' . $current_month . '</h3>';
-				}
-
-				$post_id = get_the_ID();
 				if ( is_sticky() && is_home() && ! is_paged() ) {
 					printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'notici' ) );
 				}
-				echo '<a href="' . esc_url( get_permalink() ) . '" class="event-details-link">' . get_the_title() . '</a>';
+				echo '<b>' . get_the_title() . '</b>';
 				echo '&nbsp;&nbsp;&mdash;&nbsp;&nbsp;' . get_the_excerpt();
+				echo '&nbsp;&nbsp;&nbsp;' . '<a href="' . esc_url( get_permalink() ) . '" class="event-details-link">' . 'Link' . '</a>';
 
-				// Date and times
-
-
-				echo '&nbsp;&nbsp;&mdash;&nbsp;&nbsp;' . date_i18n( $dateformat, $startdate );
-				if ( $startdate !== $enddate ) {
-					echo ' - ' . date_i18n( $dateformat, $enddate );
-				}
 
 				?>
 				</div>
