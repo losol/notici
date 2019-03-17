7772697910c757f6c9af89da76cd7e266e13d721
Ole Kristian Losvik <losolio@github.user>
2019-03-17 19:28:16 +0100

    Updated template

--- public/templates/archive-notici.php
+++ public/templates/archive-notici.php
@@ -46,8 +46,8 @@ get_header();
 					printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'notici' ) );
 				}
 				echo '<b>' . get_the_title() . '</b>';
-				echo '&nbsp;&nbsp;&mdash;&nbsp;&nbsp;' . $post->post_content;
-				echo '&nbsp;&nbsp;&nbsp;' . '<a href="' . esc_url( get_permalink() ) . '" class="event-details-link">' . 'Link' . '</a>';
+				echo $post->post_content;
+				echo '<a href="' . esc_url( get_permalink() ) . '" class="event-details-link">' . 'Link' . '</a>';
 
 
 				?>
